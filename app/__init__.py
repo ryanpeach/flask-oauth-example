0464bd06398b4e8d865f93a06bf0bd05a53c17c7
Ryan Peach <rgpeach10@gmail.com>
2017-05-31 00:19:35 +0000

    OAuth working, old models imported, new database.

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,7 @@ from flask_login import LoginManager
 from flask import Flask
     
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'top secret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-#app.config.from_object('config')
+app.config.from_object('config')
 app.config.from_object('secrets')
 
 db = SQLAlchemy(app)
